f5d37866191466d5d56930e16be3d5acdef0c0a5
Gilyoung Kim <roadzero@kodebox.io>
2018-05-30 00:51:27 +0900

    Implemented contribute page.

--- app/scripts/contribute.js
+++ app/scripts/contribute.js
@@ -1,4 +1,44 @@
 import $ from 'jquery'
+import * as memont from 'moment'
 
-$(document).ready(() => {
-})
+function appendDom (issue) {
+  var githubItemGroup = $('#github-item-group')
+  var githubItem = $('<div>', { class: 'github-item' })
+  var githubItemIcon = $('<div>', { class: 'github-item-icon' })
+  githubItemIcon.append($('<i>', { class: 'fa fa-check-circle-o fa-2x', 'aria-hidden': true }))
+  var githubItemTitle = $('<a>', { class: 'github-item-title', href: issue.html_url, target: '_blank' })
+  githubItemTitle.text(issue.title)
+  var githubItemDescription = $('<div>', { class: 'github-item-description' })
+  var dateString = memont(issue.created_at).format('DD MMM, YYYY')
+  githubItemDescription.text('#' + issue.number + ' opened ' + dateString)
+  githubItem.append(githubItemIcon)
+  githubItem.append(githubItemTitle)
+  githubItem.append(githubItemDescription)
+  githubItemGroup.append(githubItem)
+}
+
+function appendEmptyDom () {
+  var githubItemGroup = $('#github-item-group')
+  githubItemGroup.append('<p>Not existed yet.</p>')
+}
+
+async function getIssue () {
+  let result
+  try {
+    result = await $.get('https://api.github.com/repos/paritytech/parity/issues?labels=P0-dropeverything')
+    console.log(result)
+    if (result.length > 0) {
+      var count = Math.min(result.length, 3)
+      for (var i = 0; i < count; i++) {
+        appendDom(result[i])
+      }
+    } else {
+      appendEmptyDom()
+    }
+  } catch (error) {
+    console.log(error)
+    appendEmptyDom()
+  }
+}
+
+getIssue()
