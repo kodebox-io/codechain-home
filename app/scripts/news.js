27809c33b695795ad0c88df5e72486979a6e689f
Gilyoung Kim <roadzero@kodebox.io>
2018-06-05 11:40:48 +0900

    Update ui of tech blog.

--- app/scripts/news.js
+++ app/scripts/news.js
@@ -1,32 +1,25 @@
 import $ from 'jquery'
-import * as _ from 'underscore'
+// import * as _ from 'underscore'
 
 function appendDom (item) {
   var newsItemGroup = $('#new-item-group-row')
-  var newsItemContainer = $('<div>', { class: 'col-lg-4' })
-  var newsItemLink = $('<a>', { href: item.link, target: '_blank' })
+  var newsItemContainer = $('<div>', { class: 'col-lg-6' })
   var newsItem = $('<div>', { class: 'item' })
-  var mainImgContainer = $('<div>', { class: 'img-container' })
-  var mainImg = $('<img>', { src: item.thumbnail })
   var title = $('<p>', { class: 'title' })
-  var description = $('<p>', { class: 'description' })
-  // var cleanDescription = item.description.replace(/<\/?[^>]+(>|$)/g, '')
-
-  var name = $('<p>', { class: 'name' })
-  var time = $('<p>', { class: 'time' })
+  var description = $('<p>', { class: 'description block-with-text' })
+  var cleanDescription = item.description.replace(/<\/?[^>]+(>|$)/g, '')
+  description.text(cleanDescription)
+  var name = $('<p>', { class: 'name-date' })
+  var readMore = $('<a>', { class: 'read-more', href: item.link, target: '_blank' })
+  readMore.text('Read more')
 
   title.text(item.title)
-  description.text('CodeChain')
-  name.text(item.author)
-  time.text(item.pubDate)
-  mainImgContainer.append(mainImg)
-  newsItem.append(mainImgContainer)
+  name.text(item.author + ' - ' + item.pubDate)
   newsItem.append(title)
-  newsItem.append(description)
   newsItem.append(name)
-  newsItem.append(time)
-  newsItemLink.append(newsItem)
-  newsItemContainer.append(newsItemLink)
+  newsItem.append(description)
+  newsItem.append(readMore)
+  newsItemContainer.append(newsItem)
   newsItemGroup.append(newsItemContainer)
 }
 
@@ -39,16 +32,17 @@ async function getMedium () {
   let result
   try {
     result = await $.get('https://api.rss2json.com/v1/api.json', {
-      rss_url: 'https://medium.com/feed/codechain'
+      rss_url: 'https://medium.com/feed/codechain?limit=100'
     })
     if (result && result.items && result.items.length > 0) {
       var items = result.items
-      var filteredItem = _.filter(items, (item) => {
-        return _.contains(item.categories, 'chaincode')
-      })
-      var count = Math.min(filteredItem.length, 3)
+      // var filteredItem = _.filter(items, (item) => {
+      //   return _.contains(item.categories, 'chaincode')
+      // })
+      // var count = Math.min(filteredItem.length, 3)
+      var count = Math.min(items.length, 6)
       for (var i = 0; i < count; i++) {
-        appendDom(filteredItem[i])
+        appendDom(items[i])
       }
     } else {
       appendEmptyDom()
