310cfe8a231c437a581bdebf5c877852dc8b40b9
Gilyoung <roadzero@kodebox.io>
2018-12-24 12:19:48 +0900

    Remove unused componenets

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.module.scss";
+
+class App extends Component {
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">header</header>
+            </div>
+        );
+    }
+}
+
+export default App;
