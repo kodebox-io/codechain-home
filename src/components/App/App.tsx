ba8b9be8a5017e6db882cf42b520533a331d3174
Gilyoung <roadzero@kodebox.io>
2018-12-24 12:40:05 +0900

    Add react router and 404 page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import { Button } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../home/home";
+import NotFound from "../NotFound/NotFound";
 import "./App.module.scss";
 
 class App extends Component {
     public render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <span>test</span>
-                    <Button>hi</Button>
-                </header>
-            </div>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <span>test</span>
+                        <Button>hi</Button>
+                    </header>
+                    <Switch>
+                        <Route exact={true} path="/" component={Home} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/components/home/home.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default class Home extends React.Component<any, any> {
+    public render() {
+        return <div>Home</div>;
+    }
+}
