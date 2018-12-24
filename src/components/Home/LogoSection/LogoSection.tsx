addc7c451200302403fbb5ddd19cc97abcf78a9e
Gilyoung <roadzero@kodebox.io>
2018-12-24 15:36:29 +0900

    Implement the logo section of the home page

--- src/components/Home/LogoSection/LogoSection.tsx
+++ src/components/Home/LogoSection/LogoSection.tsx
@@ -1,11 +1,29 @@
 import * as React from "react";
+import laptop from "./img/laptop.png";
 import "./LogoSection.scss";
 
 export default class LogoSection extends React.Component<any, any> {
     public render() {
         return (
-            <div className="Logo-section" style={{ height: 1000 }}>
-                logo section
+            <div className="Logo-section">
+                <div className="logo-content">
+                    <div className="title">
+                        <span>CodeChain</span>
+                    </div>
+                    <div className="description">
+                        <span>
+                            an end-to-end solution for real-world asset
+                            digitization
+                        </span>
+                    </div>
+                    <div className="d-flex justify-content-center">
+                        <div className="paper-btn">White Paper</div>
+                    </div>
+                </div>
+                <div className="graph-background" />
+                <div className="computer-continer">
+                    <img src={laptop} />
+                </div>
             </div>
         );
     }
