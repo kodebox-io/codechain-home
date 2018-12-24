2e98288b363bd783b913b63208ef4ed7ce36dfff
Gilyoung <roadzero@kodebox.io>
2018-12-24 16:44:31 +0900

    Implement the platform section of the home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import FeatureSection from "./FeatureSection/FeatureSection";
 import LogoSection from "./LogoSection/LogoSection";
+import PlatformSection from "./PlatformSection/PlatformSection";
 
 export default class Home extends React.Component<any, any> {
     public render() {
@@ -8,6 +9,7 @@ export default class Home extends React.Component<any, any> {
             <div>
                 <LogoSection />
                 <FeatureSection />
+                <PlatformSection />
             </div>
         );
     }
---
+++ src/components/Home/PlatformSection/PlatformSection.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import Container from "reactstrap/lib/Container";
+import "./PlatformSection.scss";
+
+import network from "./img/Blockchain_network.svg";
+import consoleImg from "./img/Console.svg";
+import exchange from "./img/Exchange.svg";
+import explorer from "./img/Explorer.svg";
+import liquidity from "./img/Liquidity_provider.svg";
+import wallet from "./img/Wallet.svg";
+
+export default class PlatformSection extends React.Component<any, any> {
+    public render() {
+        return (
+            <div className="Platform-section">
+                <Container>
+                    <div className="title-contaienr">
+                        <span className="title">Platform</span>
+                    </div>
+                    <div>
+                        <div className="platform-item d-inline-block">
+                            <div className="mb-3">
+                                <img src={network} />
+                            </div>
+                            <div>
+                                <span className="description">
+                                    BlockChain network
+                                </span>
+                            </div>
+                        </div>
+                        <div className="platform-item d-inline-block">
+                            <div className="mb-3">
+                                <img src={consoleImg} />
+                            </div>
+                            <div>
+                                <span className="description">Console</span>
+                            </div>
+                        </div>
+                        <div className="platform-item d-inline-block">
+                            <div className="mb-3">
+                                <img src={liquidity} />
+                            </div>
+                            <div>
+                                <span className="description">
+                                    Liquidity provider
+                                </span>
+                            </div>
+                        </div>
+                        <div className="platform-item d-inline-block">
+                            <div className="mb-3">
+                                <img src={exchange} />
+                            </div>
+                            <div>
+                                <span className="description">Exchange</span>
+                            </div>
+                        </div>
+                        <div className="platform-item d-inline-block">
+                            <div className="mb-3">
+                                <img src={wallet} />
+                            </div>
+                            <div>
+                                <span className="description">Wallet</span>
+                            </div>
+                        </div>
+                        <div className="platform-item d-inline-block">
+                            <div className="mb-3">
+                                <img src={explorer} />
+                            </div>
+                            <div>
+                                <span className="description">Explorer</span>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        );
+    }
+}
