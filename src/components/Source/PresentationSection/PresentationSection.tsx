1dfd880dd846615c5b2c5e56cf4f019dcbb2bc4c
Gilyoung <roadzero@kodebox.io>
2018-12-27 18:48:15 +0900

    Delete "slides" text from the materials

--- src/components/Source/PresentationSection/PresentationSection.tsx
+++ src/components/Source/PresentationSection/PresentationSection.tsx
@@ -14,76 +14,73 @@ interface MaterialItem {
 const MaterialData: MaterialItem[] = [
     {
         title: "iTIP",
-        description:
-            "Powering blockchain businesses with CodeChain's NFTs (Slides)",
+        description: "Powering blockchain businesses with CodeChain's NFTs",
         link:
             "https://docs.google.com/presentation/d/1FMjmllXH-UNoKJVk6ubfzlgIScdRFpYLRCHs7HjhN6g/edit?usp=sharing"
     },
     {
         title: "Proof of Ownership",
-        description:
-            "New business opportunities with CodeChain tokens (Slides)",
+        description: "New business opportunities with CodeChain tokens",
         link:
             "https://docs.google.com/presentation/d/1a0-mWOEwIWpIbxaPiRcyi91I_zOlDUC958HItqaTfh0/edit?usp=sharing"
     },
     {
         title: "CodeChain SDK",
-        description: "(Slides)",
+        description: "Introduction to CodeChain SDK",
         link:
             "https://docs.google.com/presentation/d/1Joz_b1AcmDTfyitOdG0ol_3535m2ggwfNu5L6Uqx0-g/edit?usp=sharing"
     },
     {
         title: "CodeChain developers meeting",
-        description: "Cuckoo Cycle (Slides)",
+        description: "Cuckoo Cycle",
         link:
             "https://docs.google.com/presentation/d/1SkGcBDtZHE4pvSi0qQOfuUCP-USibSelH9aAt8bSLeQ/edit?usp=sharing"
     },
     {
         title: "CodeChain Connect",
-        description: "Introduction to CodeChain (Slides)",
+        description: "Introduction to CodeChain",
         link:
             "https://docs.google.com/presentation/d/1XvcKCuIhc8eDWnAx-IneiS0a_e5CgPz31tQ-xUy978c/edit?usp=sharing"
     },
     {
         title: "CodeChain Connect",
-        description:
-            "Blockchain games and GoCryptobot: lessons learned (Slides)",
+        description: "Blockchain games and GoCryptobot: lessons learned",
         link:
             "https://docs.google.com/presentation/d/17mHkxlywJu3mmlKi0k_3-HfGSfwwi1qP6qXYoTePPV4/edit?usp=sharing"
     },
     {
         title: "CodeChain Connect",
-        description: "CodeChain Deep Dive (Slides)",
+        description: "CodeChain Deep Dive",
         link:
             "https://docs.google.com/presentation/d/1Q7--JyZ8_StcHWIquiIzOarsRh7c81YOI8ppyPC5QJA/edit?usp=sharing"
     },
     {
         title: "CodeChain Connect",
-        description: "CodeChainOpen Source and Roadmap (Slides)",
+        description: "CodeChainOpen Source and Roadmap",
         link:
             "https://docs.google.com/presentation/d/1oVBP9WCR7Oolb37Zk1gdIQ74CKtKdcsbTVzD0iqAbCI/edit?usp=sharing"
     },
     {
         title: "CodeChain Seminar",
-        description: "Interchain and Sharding (Slides)",
+        description: "Interchain and Sharding",
         link:
             "https://docs.google.com/presentation/d/1woHERW1TS4ZpSWrynKHk-RRiMl3-ZTrjKdVY3QzBY_k/edit?usp=sharing"
     },
     {
         title: "CodeChain Seminar",
-        description: "Script Language (Slides)",
+        description: "Script Language",
         link:
             "https://docs.google.com/presentation/d/1E9efuCuTLT2E7gIbfPVezuAWhJMHakHr9x8zqLpt6Dg/edit?usp=sharing'"
     },
     {
         title: "IEEE Blockchain Summit Korea",
-        description: "Crypto Game Development With CodeChain (Slides)",
+        description: "Crypto Game Development With CodeChain",
         link:
             "https://docs.google.com/presentation/d/1Ly_w1-N3ju9D1xXgXTvatIIJUCNptgPtJHOotDVZqkg/edit?usp=sharing"
     },
     {
         title: "IEEE Blockchain Summit Korea",
-        description: "CodeChain Core & Use cases (Slides)",
+        description: "CodeChain Core & Use cases",
         link:
             "https://docs.google.com/presentation/d/1WP6DDBKZ26-kwManYverS69BJQFFUUcKDtNylteZlWY/edit?usp=sharing"
     },
@@ -95,19 +92,19 @@ const MaterialData: MaterialItem[] = [
     },
     {
         title: "CodeChain Seminar",
-        description: "Blockchain Storage (Slides)",
+        description: "Blockchain Storage",
         link:
             "https://docs.google.com/presentation/d/1HtXdipYotF9DKmXJhP0c2OEgLCGEj1cxCP0jEuTeS8w/edit?usp=sharing"
     },
     {
         title: "CodeChain Seminar",
-        description: "P2P Network (Slides)",
+        description: "P2P Network",
         link:
             "https://docs.google.com/presentation/d/1qYBKe8RONh3JKdRrwvKTuY_2p7FeQEJRR1oqep5lKmo/edit?usp=sharing"
     },
     {
         title: "CodeChain Seminar",
-        description: "BFT Consensus (Slides)",
+        description: "BFT Consensus",
         link:
             "https://docs.google.com/presentation/d/10W7gKEvk_6XRIlSdiKwnwP9gVzo5Re5m_24QzLGaqvk/edit?usp=sharing"
     }
