650bef0078fc5b06f7aa6e9f24695eae116977d3
Gilyoung <roadzero@kodebox.io>
2018-12-27 12:22:50 +0900

    Implement the good-first-issue page

--- src/components/Contribute/GoodFirstIssueSection/GoodFirstIssueSection.tsx
+++ src/components/Contribute/GoodFirstIssueSection/GoodFirstIssueSection.tsx
@@ -1,8 +1,172 @@
+import axios from "axios";
+import * as _ from "lodash";
+import moment from "moment";
 import * as React from "react";
+import { Col, Container, Row } from "reactstrap";
 import "./GoodFirstIssueSection.scss";
+import check from "./img/check.svg";
 
-export default class GoodFirstIssueSection extends React.Component<any, any> {
-    public render() {
-        return <div>GoodFirstIssueSection</div>;
+const projectList = [
+    {
+        name: "CodeChain-sdk-js",
+        url: "https://api.github.com/repos/CodeChain-io/codechain-sdk-js"
+    },
+    {
+        name: "CodeChain",
+        url: "https://api.github.com/repos/CodeChain-io/codechain"
     }
+];
+
+interface State {
+    issues: {
+        [name: string]: {
+            isFetching: boolean;
+            error?: boolean | null;
+            data?:
+                | [
+                      {
+                          title: string;
+                          url: string;
+                          number: number;
+                          updatedAt: string;
+                      }
+                  ]
+                | null;
+        };
+    };
+}
+
+export default class GoodFirstIssueSection extends React.Component<any, State> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            issues: {}
+        };
+    }
+    public componentDidMount() {
+        this.fetchIssues();
+    }
+    public render() {
+        const { issues } = this.state;
+        return (
+            <div className="Good-first-issue">
+                <Container>
+                    <div className="good-first-issue-seciton-title-container text-center">
+                        <span className="section-title">Good first issue</span>
+                    </div>
+                    {_.keys(issues).map(projectName => {
+                        const issue = issues[projectName];
+                        if (issue.isFetching) {
+                            return (
+                                <div key={`${issue}`}>
+                                    <span>Loading...</span>
+                                </div>
+                            );
+                        } else if (issue.error) {
+                            return (
+                                <div key={`${issue}`}>
+                                    <p>To be uploaded</p>
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div key={`${issue}`}>
+                                    <div className="title-container">
+                                        <span className="title">
+                                            {projectName}
+                                        </span>
+                                    </div>
+                                    <div className="issue-list">
+                                        {issue.data!.map(data =>
+                                            this.getIssueItem(data)
+                                        )}
+                                    </div>
+                                </div>
+                            );
+                        }
+                    })}
+                </Container>
+            </div>
+        );
+    }
+
+    private getIssueItem = (item: {
+        title: string;
+        url: string;
+        number: number;
+        updatedAt: string;
+    }) => {
+        return (
+            <a href={item.url} target="_blank">
+                <Row className="issue-item">
+                    <Col md={7}>
+                        <div className="d-flex align-items-center">
+                            <div className="check-container">
+                                <img src={check} />
+                            </div>
+                            <div className="issue-title-container">
+                                <span>{item.title}</span>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col md={5}>
+                        <div className="text-left text-md-right">
+                            <span className="date-text">
+                                {`#${item.number} opened on ${moment(
+                                    item.updatedAt
+                                ).format("DD MMM, YYYY")}`}
+                            </span>
+                        </div>
+                    </Col>
+                </Row>
+            </a>
+        );
+    };
+
+    private fetchIssues = async () => {
+        const issuePath = "/issues?labels=good+first+issue";
+        await Promise.all(
+            projectList.map(async project => {
+                const projectName = project.name;
+                this.setState({
+                    issues: {
+                        ...this.state.issues,
+                        [projectName]: {
+                            isFetching: true
+                        }
+                    }
+                });
+                const projectURL = project.url;
+                try {
+                    const response = await axios.get(projectURL + issuePath);
+                    const issues = response.data.map((data: any) => ({
+                        title: data.title,
+                        url: data.url,
+                        number: data.number,
+                        updatedAt: data.updated_at
+                    }));
+                    this.setState({
+                        issues: {
+                            ...this.state.issues,
+                            [projectName]: {
+                                isFetching: false,
+                                data: issues
+                            }
+                        }
+                    });
+                } catch (e) {
+                    console.log(e);
+                    this.setState({
+                        issues: {
+                            ...this.state.issues,
+                            [projectName]: {
+                                isFetching: false,
+                                error: true
+                            }
+                        }
+                    });
+                }
+            })
+        );
+    };
 }
