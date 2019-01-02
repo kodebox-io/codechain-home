485ae319b2442df8df963b49551faf76d9dc06ea
Gilyoung <roadzero@kodebox.io>
2019-01-02 18:41:23 +0900

    Update the building script

--- bin/ublish_to_ghpages.sh
+++ bin/ublish_to_ghpages.sh
@@ -4,24 +4,22 @@ DIR=$(dirname "$0")
 
 cd $DIR/..
 
-echo "Deleting old publication"
-rm -rf build
-
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
 fi
 
+echo "Deleting old publication"
+rm -rf build
+
+echo "Start to build project"
 mkdir build
 git worktree prune
 rm -rf .git/worktrees/build/
 
 echo "Checking out gh-pages branch into build"
 git worktree add -B gh-pages build origin/gh-pages
-
-echo "Removing existing files"
-rm -rf build/*
 
 echo "Generating site"
 yarn install
@@ -37,3 +35,4 @@ echo "codechain.io" >> public/CNAME
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+git push origin
