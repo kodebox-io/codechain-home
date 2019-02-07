7a85d8b3a58a04030b6516979fdd84ef82dff873
Gilyoung <roadzero@kodebox.io>
2019-02-07 13:59:18 +0900

    Add the link to the talk item

--- src/components/TalksSection/TalkItem/TalkItem.tsx
+++ src/components/TalksSection/TalkItem/TalkItem.tsx
@@ -12,20 +12,22 @@ export default class TalkItem extends React.Component<Props, any> {
         const { talk } = this.props;
         return (
             <div className="Talk-item">
-                <div className="content">
-                    <div className="deco" />
-                    <div className="title-container">
-                        <span>{talk.title}</span>
-                    </div>
-                    <div className="bottom-container">
-                        <div className="date-container">
-                            <span>{talk.date}</span>
+                <a href={talk.link} target="_blank">
+                    <div className="content">
+                        <div className="deco" />
+                        <div className="title-container">
+                            <span>{talk.title}</span>
                         </div>
-                        <div className="logo-container">
-                            <img src={Logo} />
+                        <div className="bottom-container">
+                            <div className="date-container">
+                                <span>{talk.date}</span>
+                            </div>
+                            <div className="logo-container">
+                                <img src={Logo} />
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         );
     }
