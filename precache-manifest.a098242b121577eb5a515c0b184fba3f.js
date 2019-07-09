0ebaae68313dd6d96d0e7b3e82b2b567dbfdb66f
box4 <box4@kodebox.io>
2019-07-09 17:13:14 +0900

    Publishing to gh-pages (publish.sh)

--- precache-manifest.a098242b121577eb5a515c0b184fba3f.js
+++ precache-manifest.a098242b121577eb5a515c0b184fba3f.js
@@ -124,12 +124,12 @@ self.__precacheManifest = [
     "url": "/static/media/slick.ced611da.eot"
   },
   {
-    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
-    "url": "/static/media/slick.b7c9e1e4.woff"
-  },
-  {
     "revision": "d41f55a78e6f49a5512878df1737e58a",
     "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
   },
   {
     "revision": "f97e3bbf73254b0112091d0192f17aec",
